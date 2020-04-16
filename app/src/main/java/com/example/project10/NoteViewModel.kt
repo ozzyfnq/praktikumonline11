ceec30732eac0d045f07f7c2ff960a83653a4a7c
ozzyfnq <fachrurrozyn@gmail.com>
2020-04-16 15:56:04 +0700

    praktikumonline11

---
+++ app/src/main/java/com/example/project10/NoteViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.project10
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.project10.catatan.Note
+import com.example.project10.catatan.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private var repository: NoteRepository = NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+    fun update(note: Note) {
+        repository.update(note)
+    }
+    fun delete(note: Note) {
+        repository.delete(note)
+    }
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
